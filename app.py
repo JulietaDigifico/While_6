f3eeb6d4b43e5e95ee544a2eb9ab5cc0bf646076
Julieta <julieta.digifico@gmail.com>
2024-02-22 22:27:12 -0300

    Ejercicio6 While

---
+++ app.py
@@ -0,0 +1,54 @@
+import tkinter
+from tkinter.messagebox import showinfo as alert
+from tkinter.messagebox import askyesno as question
+from tkinter.simpledialog import askstring as prompt
+import customtkinter
+
+'''
+nombre: Julieta
+apellido: Digifico
+---
+Ejercicio: while_06
+---
+Enunciado:
+Al presionar el botón ‘Comenzar ingreso’, solicitar 5 números mediante prompt. 
+Calcular la suma acumulada y el promedio de los números ingresados. 
+Luego informar los resultados en las cajas de texto txt_suma_acumulada y txt_promedio
+
+'''
+
+class App(customtkinter.CTk):
+    
+    def __init__(self):
+        super().__init__()
+
+        self.title("UTN FRA")
+        
+        self.txt_suma_acumulada = customtkinter.CTkEntry(master=self, placeholder_text="Suma acumulada")
+        self.txt_suma_acumulada.grid(row=0, padx=20, pady=20)
+
+        self.txt_promedio = customtkinter.CTkEntry(master=self, placeholder_text="Promedio")
+        self.txt_promedio.grid(row=1, padx=20, pady=20)
+
+        self.btn_mostrar = customtkinter.CTkButton(master=self, text="Comenzar Ingreso", command=self.btn_comenzar_ingreso_on_click)
+        self.btn_mostrar.grid(row=2, padx=20, pady=20, columnspan=2, sticky="nsew")
+
+
+    def btn_comenzar_ingreso_on_click(self):
+        contador = 0
+        suma_acumulada = 0
+        while(contador < 5):
+            numero = prompt("prompt", "Ingrese un numero")
+            contador += 1
+            numero = int(numero)
+            suma_acumulada += numero
+        promedio = suma_acumulada / contador
+
+        self.txt_promedio.insert(0, round (promedio,1))
+        self.txt_suma_acumulada.insert(0, suma_acumulada)
+
+    
+if __name__ == "__main__":
+    app = App()
+    app.geometry("300x300")
+    app.mainloop()
